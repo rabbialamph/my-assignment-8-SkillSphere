62af22fcd283d91e4d787dbafe4bd31f2dab07cd
rabbialamph <mdrabbialom98@gmail.com>
2026-05-03 17:34:51 +0600

    deploy

--- src/lib/auth.jsx
+++ src/lib/auth.jsx
@@ -2,33 +2,18 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const uri = process.env.AUTH_DB_URI;
+const client = new MongoClient(process.env.AUTH_DB_URI);
 
-if (!uri) {
-  throw new Error("❌ AUTH_DB_URI is missing");
-}
-
-const client = new MongoClient(uri);
-
-let db;
-
-async function getDB() {
-  if (!db) {
-    await client.connect();
-    db = client.db("better_auth_db_assignment8");
-  }
-  return db;
-}
+const db = client.db('better_auth_db_assignment8');
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL,
-  secret: process.env.BETTER_AUTH_SECRET,
 
-  emailAndPassword: {
-    enabled: true,
-  },
+  emailAndPassword: { 
+    enabled: true, 
+   }, 
 
-  database: mongodbAdapter(await getDB(), {
-    client,
+    database: mongodbAdapter(db, {
+
+    client
   }),
 });
