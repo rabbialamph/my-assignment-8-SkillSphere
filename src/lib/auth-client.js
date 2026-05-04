f825d4e44eac7a5d5207c3d60f2c3bc0ee5d6d7e
rabbialamph <mdrabbialom98@gmail.com>
2026-05-04 14:52:30 +0600

    URL Change Add Varcel Deploy Live ULR

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://my-assignment-8-skill-sphere-6124.vercel.app"
 });
 
 export const {
